f1ee6765a0874f87e7c489fc396b7eaef6377892
Chris Ackerman <christopherackerman104@gmail.com>
2020-12-10 09:29:31 -0500

    added Luna's files for q9

---
+++ assignments/202a_hw2/MacroHW2_Q9.m
@@ -0,0 +1,16 @@
+readtable('simulated_log_linear_economy.txt');
+
+%%drop first 100 obs
+ans(1:101,:)=[]; 
+c = ans{:, 2};
+
+%% Fit the model
+% Set model 1 AR lags, 0 multiplicative components, and 0 MA lags
+Mdl2 = arima(1, 0, 0); 
+EstMdl2 = estimate(Mdl2, c);
+
+%infer the residuals
+res=infer(EstMdl2, c);
+
+%plot autocorrelation in residuals;
+autocorr(res);
