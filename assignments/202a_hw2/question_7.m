8830db50c6b46fac95d000c354dd48c1f39aea23
Ben Pirie <btpirie@berkeley.edu>
2020-12-10 08:41:26 -0800

    removed extraneous code

--- assignments/202a_hw2/question_7.m
+++ assignments/202a_hw2/question_7.m
@@ -6,7 +6,6 @@ n = 1; % number of forward-looking variables: c
 barR = 1.05;
 ItoYratio = 0.2;
 RKtoYratio = 0.3;
-ybar = 1;
 CtoYratio = 1 - ItoYratio;
 
 % calibration
@@ -56,46 +55,3 @@ end
 if(sum(abs(diag(Lambda)) > 1) == n)
     disp("By Blanchard Khan proposition 1, we have a unique solution.");
 end
-
-% partition matrices
-Gammainv = inv(Gamma);
-G11 = Gammainv(1 : m, 1 : n);
-G12 = Gammainv(1 : m, n + 1 : m + n);
-G21 = Gammainv(m + 1 : m + n, 1 : n);
-G22 = Gammainv(m + 1 : m + n, n + 1 : m + n);
-Lambda1 = Lambda(1 : m , 1 : m);
-Lambda2 = Lambda(m + 1 : m + n, m + 1 : m + n);
-
-% state variables solution
-% E x_t+t = H*x_t
-H = inv(-G11 * inv(G21) * G22 + G12) * Lambda1 * (-G11 * inv(G21) * G22+G12);
-% Jump Variables : Policy function
-THETA = -inv(G21) * G22;
-
-% Simulation
-Mtilde11 = M11;
-Mtilde12 =[M12, M13];
-Mtilde21 = [M21 ;M31];
-Mtilde22 = [M22, M23; M32, M33];
-A12=Mtilde11 * THETA + Mtilde12;
-A22=Mtilde21 * THETA + Mtilde22;
-A=[zeros(n, size(n, n)), A12 ; 
-   zeros(m, n), A22 ];
-T =100; % number of periods
-x2v(1, 1) = 0;
-x2v(1, 2) = 0;
-x1v(1, 1) = THETA * x2v (1 ,:)';
-X = zeros(T, m + n);
-error = normrnd(0, 0.01, [T, 1]);
-X(1, :) = [x1v ,x2v];
-for i = 2 : 1 : T
-X(i ,:) = (A * X(i - 1, :)' + [0, 0, error(i)]')';
-end
-GDP = Yss * exp((X(:, 2) * ttheta + X(:, 3)));
-plot(linspace(0, T, T), GDP, 'LineWidth', 3, 'Color', 'k')
-grid on
-xlabel('Time')
-ylabel('GDP t')
-hold on
-plot(linspace(0, T, T), GDP * 0 + Yss, 'LineWidth', 3, 'Color', 'r')
-legend('GDP (levels)', 'Yss')
